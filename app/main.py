49333fe70861ad8f8b86e8bfd69b58519ff50be2
Song.Truong <truongminhsong@gmail.com>
2018-07-30 00:07:00 +0700

    Remove compiled files

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from app.api.users import UserController
 from app.errors import AppError
 from app.middleware import JSONTranslator, DatabaseSessionManager, AuthHandler
 from app.database import db_session, init_session
-from app.api.customer import CustomerCreate,CustomerList,CustomerDelete,CustomerDetail,CustomerUpdate
+from app.api.customer import CustomerCreateAPI,CustomerAPI,CustomerList
 
 init_session()
 middleware = [AuthHandler(db_session), JSONTranslator(), DatabaseSessionManager(db_session)]
@@ -17,9 +17,7 @@ application.add_route('/', BaseResource())
 application.add_route('/auth/login', Auth())
 application.add_route('/user/create', UserController())
 
-application.add_route('/customer/create', CustomerCreate())
-application.add_route('/customer/update', CustomerUpdate())
-application.add_route('/customer/delete', CustomerDelete())
-application.add_route('/customer/detail/{_id}', CustomerDetail())
+application.add_route('/customer', CustomerCreateAPI())
+application.add_route('/customer/{_id}', CustomerAPI())
 application.add_route('/customer/list', CustomerList())
 application.add_error_handler(AppError, AppError.handle)
